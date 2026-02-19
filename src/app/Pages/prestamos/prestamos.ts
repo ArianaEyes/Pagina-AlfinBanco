4250e716186d5f51a65c4419eeb734ac6b245c9f
Ariana1825 <apreyes18@outlook.com>
2026-02-18 22:37:46 -0500

    actualizeishon del resposiveishon

--- src/app/Pages/prestamos/prestamos.ts
+++ src/app/Pages/prestamos/prestamos.ts
@@ -8,7 +8,7 @@ import { Config } from '../../Model/config';
   imports: [CommonModule],
   standalone: true,
   templateUrl: './index.html',
-  styleUrl: './prestamos.css',
+  styleUrls: ['./prestamos.css'],
 })
 export class Prestamos {
   private configService = inject(ConfigService);
@@ -27,6 +27,50 @@ export class Prestamos {
       this.config = configs[0];
       this.cargarFormulario();
     });
+
+    this.checkScreen();
+    window.addEventListener('resize', () => this.checkScreen());
+  }
+
+  items = [
+    {
+      titulo: 'De S/1,500 a S/40,000',
+      img: '/icons/icon1.svg',
+      descripcion: 'Accede a cualquier monto que necesites desde 1,500 soles hasta 40,000 soles.',
+      height: '25%',
+    },
+    {
+      titulo: 'De 12 a 48 meses',
+      img: '/icons/icon2.svg',
+      descripcion: 'Elige el plazo que más te convenga para poder pagar y cancelar tu préstamo.',
+      height: '25%',
+    },
+    {
+      titulo: 'Hasta 45 días',
+      img: '/icons/icon3.svg',
+      descripcion: 'Recibe total flexibilidad para iniciar el pago de tus cuotas de préstamo.',
+      height: '22%',
+    },
+  ];
+  currentIndex = 0;
+  intervalId: any;
+  isMobile = false;
+
+  checkScreen() {
+    this.isMobile = window.innerWidth < 413;
+
+    if (this.isMobile && !this.intervalId) {
+      this.startAutoPlay();
+    }
+    if (!this.isMobile && this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+  startAutoPlay() {
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.items.length;
+    }, 3000);
   }
 
   cargarFormulario() {
